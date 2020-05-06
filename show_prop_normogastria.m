d427b2ec75aad11407f8a25a142964887f7b9134
niwolpert <50142717+niwolpert@users.noreply.github.com>
2020-05-06 11:46:45 +0200

    Update show_prop_normogastria.m

--- show_prop_normogastria.m
+++ show_prop_normogastria.m
@@ -7,8 +7,9 @@ function show_prop_normogastria(EGG_filtered)
 %     EGG_filtered  filtered EGG signal (output from 'compute_filter_EGG.m')
 % 
 % When using this function in any published study, please cite: Wolpert, 
-% N., Rebollo, I., Tallon-Baudry, C. (2019). Electrogastrography for 
-% psychophysiological research: practical guidelines and normative data.
+% N., Rebollo, I., Tallon-Baudry, C. (2020). Electrogastrography for 
+% psychophysiological research: practical considerations, analysis pipeline 
+% and normative data in a large sample. Psychophysiology (in press)
 %
 % This function was written in Matlab version R2017b.
 %
